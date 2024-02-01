4cd2a6b4c9317e3855ff880f36f3b9f557e5726f
idkwhatshouldinamemyself <158302160+idkwhatshouldinamemyself@users.noreply.github.com>
2024-02-01 22:54:03 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,22 @@
-require(11560761226).HD("alllaudhhf")
+local function Test(plr)
+  local tool = instance.new("Tool")
+  tool.parent = plr.Backpack
+  tool.Name = "Weapon"
+
+  local handle = instance.new("Part")
+  handle.parent = tool
+  handle.Name = "Handle"
+  handle.Size = Vector3.new(10,10,10)
+  handle.Anchor = false
+  handle.Transparency = 1
+
+  tool.Activate:Connect(function(plr)
+      for _,v in pair(game.workspace:GetChildren() do
+          if v == Humanoid and v.Parent ~= plr then
+            Humanoid.Health = Humanoid.Health-Humanoid.Health
+          end
+      end
+   end)
+  end
+
+  
